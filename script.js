5fd7d7440173937d4bb489dac0a0722a7ffa0539
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-02-13 20:08:22 +0100

    Varibale declaration with LET and CONST

---
+++ script.js
@@ -0,0 +1,59 @@
+/////////////////////////-------------- LET and CONST -----------------------/////////////////
+
+/*
+//ES5
+var name5 = 'Jane Smith';
+var age5 = 23;
+name5 = 'Jane Miller';
+console.log(name5);
+
+
+// ES6
+const name6 = 'Jane Smith';
+let age6 = 23;
+name6 = 'Jane Miller';
+console.log(name6);
+*/
+
+
+// ES5
+function driversLicence(passedTest) {
+
+    if (passedTest) {
+        console.log(firstName);
+        var firstName = 'John';
+        var yearOfBirth = 1990;
+    }
+
+    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+}
+
+driversLicence(true);
+
+
+
+// ES6
+function driversLicence(passedTest) {
+
+    // console.log(firstName);
+    let firstName;
+    const yearOfBirth = 1990;
+
+
+    if (passedTest) {
+        let firstName = 'John';
+    }
+
+    console.log(firstName + ', born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
+}
+
+driversLicence(true);
+
+
+let i = 23;
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+console.log(i);
