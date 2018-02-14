8b6a922c7b5916e9da248a1bb3e4e8a14c175829
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-02-14 18:17:23 +0100

    CLASSES with SUBCLASSES -- How to connect two function constructors in ES6

--- script.js
+++ script.js
@@ -602,3 +602,38 @@ var johnAthlete5 = new Athlete5('John', 1990, 'Swimmer', 3, 10);
 
 johnAthlete5.calculateAge();
 johnAthlete5.wonMedal();
+
+
+
+// ES6
+class Person6 {
+    constructor (name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+
+    calculateAge() {
+        var age = new Date().getFullYear() - this.yearOfBirth;
+        console.log(age);
+    
+    }
+}
+
+class Athlete6 extends Person6 {
+    constructor(name, yearOfBirth, job, olympicGames, medals) {
+        super(name, yearOfBirth, job);
+        this.olympicGames = olympicGames;
+        this.medals = medals;
+    }
+
+    wonMedal() {
+        this.medals++;
+        console.log(this.medals);
+    }
+}
+
+const johnAthlete6 = new Athlete6('John', 1990, 'Swimmer', 3, 10);
+
+johnAthlete6.wonMedal();
+johnAthlete6.calculateAge();
