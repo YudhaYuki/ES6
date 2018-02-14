23d1a084dca6503d89b1a22607345b851f7b61b3
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-02-14 11:35:49 +0100

    Joining TWO ARRAYE with SPREAD methods in S6

--- script.js
+++ script.js
@@ -370,3 +370,10 @@ console.log(sum2);
 // ES6
 const sum3 = addFourAges(...ages);
 console.log(sum3);
+
+
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+const bigFamily = [...familySmith,'Lily', ...familyMiller];
+
+console.log(bigFamily);
